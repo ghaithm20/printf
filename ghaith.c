ecd4537a0ade52bdb8abeb65be40de02ad490cfb
ghaithm20 <ghaithm20@gmail.com>
2024-02-02 05:17:37 -0800

    intial the c file

---
+++ ghaith.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * _printf - Custom printf function
+ * @format: Format string containing conversion specifiers
+ * Return: Number of characters printed (excluding the null byte)
+ */
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int count = 0;
+
+    va_start(args, format);
+
+    while (*format != '\0')
+    {
+        if (*format == '%' && *(format + 1) != '\0')
+        {
+            format++; // Move past '%'
+            switch (*format)
+            {
+                case 'c':
+                    count += write(1, &va_arg(args, int), 1);
+                    break;
+
+                case 's':
+                    count += write(1, va_arg(args, char *), 1);
+                    break;
+
+                case '%':
+                    count += write(1, "%", 1);
+                    break;
+
+                default:
+                    count += write(1, "%", 1);
+                    count += write(1, format, 1);
+            }
+        }
+        else
+        {
+            count += write(1, format, 1);
+        }
+
+        format++;
+    }
+
+    va_end(args);
+
+    return count;
+}
+
