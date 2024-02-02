d0c20fd2711f26fc07403cda77b518bb4e390201
ghaithm20 <ghaithm20@gmail.com>
2024-02-02 05:39:09 -0800

    update prinf.c file

--- printf.c
+++ printf.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdarg.h>
-#include <unistd.h>
 
 /**
  * _printf - Custom printf function
@@ -11,8 +10,8 @@ int _printf(const char *format, ...)
 {
     va_list args;
     int count = 0;
-    char c;
     char *str;
+    int num;
 
     va_start(args, format);
 
@@ -24,8 +23,7 @@ int _printf(const char *format, ...)
             switch (*format)
             {
                 case 'c':
-                    c = va_arg(args, int);
-                    count += write(1, &c, 1);
+                    count += putchar(va_arg(args, int));
                     break;
 
                 case 's':
@@ -34,23 +32,29 @@ int _printf(const char *format, ...)
                         str = "(null)";
                     while (*str != '\0')
                     {
-                        count += write(1, str, 1);
+                        count += putchar(*str);
                         str++;
                     }
                     break;
 
+                case 'd':
+                case 'i':
+                    num = va_arg(args, int);
+                    count += write_number(num);
+                    break;
+
                 case '%':
-                    count += write(1, "%", 1);
+                    count += putchar('%');
                     break;
 
                 default:
-                    count += write(1, "%", 1);
-                    count += write(1, format, 1);
+                    count += putchar('%');
+                    count += putchar(*format);
             }
         }
         else
         {
-            count += write(1, format, 1);
+            count += putchar(*format);
         }
 
         format++;
@@ -59,5 +63,50 @@ int _printf(const char *format, ...)
     va_end(args);
 
     return count;
+}
+
+/**
+ * write_number - Write an integer to standard output
+ * @num: Integer to be written
+ * Return: Number of characters written
+ */
+int write_number(int num)
+{
+    int count = 0;
+    char digit;
+
+    // Handle negative numbers
+    if (num < 0)
+    {
+        count += putchar('-');
+        num = -num;
+    }
+
+    // Write each digit to standard output
+    if (num == 0)
+    {
+        count += putchar('0');
+    }
+    else
+    {
+        while (num > 0)
+        {
+            digit = '0' + (num % 10);
+            count += putchar(digit);
+            num /= 10;
+        }
+    }
+
+    return count;
+}
+
+/**
+ * putchar - Write a character to standard output
+ * @c: Character to be written
+ * Return: 1 (success), EOF (failure)
+ */
+int putchar(int c)
+{
+    return write(1, &c, 1);
 }
 
